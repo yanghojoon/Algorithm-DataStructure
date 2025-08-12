87963b8405629e5ae44a070781d191674f9ccdb6
yanghojoon <yanghojoon8487@gmail.com>
2025-08-12 20:22:59 +0900

    feat: Merge Sort / Quick Sort 구현

--- Stack Start/Sources/Sort.swift
+++ Stack Start/Sources/Sort.swift
@@ -52,3 +52,43 @@ func bubbleSort<T: Comparable>(_ array: [T]) -> [T] {
     
     return sortedArray
 }
+
+// MARK: Merge Sort
+/// 시간 복잡도 : nLogn -> logn의 경우 절반으로 계속 쪼개는 과정에서 발생 / 병합과정에서 정렬을 하면서 다시 합치는 과정이 n
+func mergeSort<T: Comparable>(list: [T]) -> [T] {
+    guard list.count > 1 else {
+        return list
+    }
+
+    let leftList = Array(list[0..<list.count / 2])
+    let rightList = Array(list[list.count / 2..<list.count])
+
+    return merge(left: mergeSort(list: leftList), right: mergeSort(list: rightList)) // 재귀로 돌면서 계속 쪼갤 수 있도록 함 (가장 작은 단위까지 쪼갠 후 정렬하며 병합됨)
+}
+
+func merge<T: Comparable>(left: [T], right: [T]) -> [T] {
+    var mergedList = [T]()
+    var left = left
+    var right = right
+
+    while left.count > 0 && right.count > 0 {
+        if left.first! < right.first! { // 비교해서 작은 값을 넣는 과정
+            mergedList.append(left.removeFirst())
+        } else {
+            mergedList.append(right.removeFirst())
+        }
+    }
+
+    return mergedList + left + right
+}
+
+// MARK: Quick Sort // 요소가 다 겹칠 때 요소가 하나로 나오게 됨
+func quickSort<T: Comparable>(_ array: [T]) -> [T] {
+    guard let first = array.first, array.count > 1 else { return array }
+
+    let pivot = first // 첫 번째 값부터 기준점을 잡고
+    let left = array.filter { $0 < pivot } // 그거보다 작으면 왼쪽에 모아둠 (이때 정렬을 하진 않음 - 배열 순서대로)
+    let right = array.filter { $0 > pivot } // 그거보다 크면 오른쪽에 모아둠
+
+    return quickSort(left) + [pivot] + quickSort(right) // 재귀로 쪼갤 값이 있을 때까지 쪼개며 첫 번째 값 기준으로 정렬을 해나감
+}
